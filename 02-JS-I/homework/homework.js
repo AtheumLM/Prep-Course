3d0219d2c20c9ffe549ba32a4cf8f41bad346a20
lemolina <lautaro677@gmail.com>
2022-10-22 22:10:40 -0300

    Homework JSI OK

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,22 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola";
+const nuevaString = "Mi Nombre es Lautaro Molina";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 3;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,38 +28,51 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  var nombre = str;
+  return nombre;
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  var suma = x + y;
+  return suma;
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  var resta = x - y;
+  return resta;
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  var producto = x * y;
+  return producto;
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  var division = x / y;
+  return division;
+
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (x === y) {
+    return true;
+  }
+  return false
   
 }
 
@@ -67,6 +80,13 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  var l1 = str1.length;
+  var l2 = str2.length;
+
+  if (l1===l2){
+    return true;
+  }
+  return false;
   
 }
 
@@ -74,71 +94,92 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num < 90) {
+    return true;
+  }
+  return false;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {
+    return true;
+  }
+  return false;
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  var resto = x%y;
+  return resto;
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  var resto = num%2;
+  if (resto === 0){
+    return true;
+  }
+  return false;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  var resto = num%2;
+  if (resto !== 0){
+    return true;
+  }
+  return false;
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  var resultado = Math.pow(num,2);
+  return resultado;
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  var resultado = Math.pow(num,3);
+  return resultado;
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  var resultado = Math.pow(num,exponent);
+  return resultado;
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  var numero = Math.round(num);
+  return numero;
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  var numero = Math.ceil(num);
+  return numero;
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  var numram = Math.random();
+  return numram;
 }
 
 function esPositivo(numero) {
@@ -146,46 +187,65 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  var pos = "Es positivo";
+  var neg = "Es negativo";
+
+  if (numero > 0) {
+    return pos;    
+  }
+  if (numero < 0) {
+    return neg;
+  }
+  return false
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var newStr = str + '!';
+  return newStr;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var newStr = nombre + ' ' +  apellido;
+  return newStr;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var str1 = "Hola ";
+  var str2 = "!"
+  var newStr = str1 + nombre + str2;
+  return newStr;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var area = alto * ancho;
+  return area;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perimetro = lado * 4;
+  return perimetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  var area = (base * altura)/2;
+  return area;
 
 }
 
@@ -194,7 +254,8 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var conversion = euro * 1.2;
+  return conversion;
 }
 
 
@@ -204,6 +265,27 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  var largo = letra.length;
+  const error = "Dato incorrecto";
+  const vocal = "Es vocal";
+  if (largo === 1) {
+    if (letra === "a") {
+      return vocal;
+    }
+    if (letra === "e") {
+      return vocal;
+    }
+    if (letra === "i") {
+      return vocal;
+    }
+    if (letra === "o") {
+      return vocal;
+    }
+    if (letra === "u") {
+      return vocal;
+    }
+  }
+  return error
   
 }
 
---
+++ 02-JS-I/homework/tests/JS-1.md
@@ -0,0 +1,17 @@
+Variables: Son espacios de memorias destinados a guardar datos, la comparación mas sencillas es pensar que son "Cajas", cajas donde podemos almacenar datos.
+
+String: Se define asi a una cadena de texto, la cual es inmutable, compuesta por caracteres espciales y alfanumericos entre "".
+
+Funciones: Son bloques de codigos creados bajo un nombre especifico que nosotros asignamos, para luego mediante este nombre predeterminado invocarlas y que se ejecute el codigo que contienene.
+
+Su estructura es 
+
+1. Function: Keyword destinada a la creación de una funcion.
+2. Nombre definido para la función.
+3. () Se colocarian cariables, que son argumentos requeridos por el codigo que contiene la función
+4. Return: Keyword destinada a extraer datos del condigo contenido en un función,
+Normalmente lo que pasa dentro de una función es desconocido e inaccesible por el resto del programa, para salvar esto debemos usar "return" y especificar los datos/variables que queremos que la función entregue al programa main.
+
+IF: es un controlador de flujo, el mismo nos permite evaluar una condicion predispuesta para luego optar por uno de dos caminos posibles dentro del codigo.
+
+Valores Booleanos: Estos son True(Verdadero) y False(Falso), vienens de la logica binaria 0 y 1, los mismos nos ayudan a trabajar con los controladores de flujo aplicando operadores condicionales.
